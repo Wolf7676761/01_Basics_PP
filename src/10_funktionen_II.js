81a81fb5d0224e7672920a3660db66fdf90a3870
Wolf7676761 <felixwolf93@web.de>
2025-11-12 10:10:56 +0000

    Update

---
+++ src/10_funktionen_II.js
@@ -0,0 +1,23 @@
+
+
+/*** Rechner  */
+/*
+0. a+b | a*b | a/b --> Ergebnis C
+
+1. Dateneingabe + -überprüfung :
+2. Auswahl Rechenart :
+3. Fkt. Grundrechenarten :
+4. Ausgabe in Konsole : check!
+*/
+
+// module: addition a + b |  test:
+output(add(2,2));
+output(add(2,-2));
+output(add(2,0));
+function add(a,b) {
+    return a + b; 
+}
+
+// module: output | test : 
+function output(outputData) {    
+}
